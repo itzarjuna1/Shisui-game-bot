5b801d9a85854f8a221d7cec5c233ca1b634731a
ARJUN <pachangedinanath@gmail.com>
2025-12-13 01:04:29 +0530

    Delete plugin/music/apple_music.py

--- plugin/music/apple_music.py
+++
@@ -1,7 +0,0 @@
-# Apple Music does not provide direct free streams
-# We search Apple Music track on YouTube
-from plugins.music.youtube import get_youtube_audio
-
-def get_apple_music_audio(track_name: str, artist: str):
-    query = f"{track_name} {artist} site:youtube.com"
-    return get_youtube_audio(query)
