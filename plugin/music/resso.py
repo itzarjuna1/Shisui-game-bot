5a166ef27ae675a9062b0c26e847a66ba412c68f
ARJUN <pachangedinanath@gmail.com>
2025-12-13 01:05:06 +0530

    Delete plugin/music/resso.py

--- plugin/music/resso.py
+++
@@ -1,5 +0,0 @@
-from plugins.music.youtube import get_youtube_audio
-
-def get_resso_audio(track_name: str, artist: str):
-    query = f"{track_name} {artist} site:youtube.com"
-    return get_youtube_audio(query)
